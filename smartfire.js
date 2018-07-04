f69e394a1bf66300047255a2bcaff511b292c5ea
dave morgan <morgo52@gmail.com>
2018-07-04 22:19:40 +1000

    compile ts

--- smartfire.js
+++ smartfire.js
@@ -3,19 +3,19 @@ console.log('Invasion successful!');
 // Start
 updatePageSidemenu();
 function updatePageSidemenu() {
-    console.log('execute - updatePageSidemenu');
+    // console.log('execute - updatePageSidemenu');
     removeStupidMeatBackground();
     createHomeLink();
     createboobyModeLink();
 }
 function removeStupidMeatBackground() {
-    console.log('execute - removeStupidMeatBackground');
+    // console.log('execute - removeStupidMeatBackground');
     var styleElement = document.createElement('style');
     styleElement.innerHTML = '.header-background-image { background-image: none !important; height: auto !important; }';
     document.querySelector('.header-background-image').appendChild(styleElement);
 }
 function createHomeLink() {
-    console.log('execute - createHomeLink');
+    // console.log('execute - createHomeLink');
     var homeLink = document.querySelector('.page-sidemenu ion-list > button').cloneNode(true);
     homeLink.addEventListener('click', function () { window.location.href = 'https://app.smartfirebbq.com/#/home'; }, false);
     homeLink.childNodes[0].childNodes[0].childNodes[1].childNodes[2].nodeValue = 'Home';
@@ -25,7 +25,7 @@ function createHomeLink() {
     document.querySelector('.page-sidemenu ion-list > button ion-icon').classList.add('ion-md-home');
 }
 function createboobyModeLink() {
-    console.log('execute - createboobyModeLink');
+    // console.log('execute - createboobyModeLink');
     var boobyLink = document.querySelector('.page-sidemenu ion-list > button').cloneNode(true);
     boobyLink.addEventListener('click', function () { createBoobs(); }, false);
     boobyLink.childNodes[0].childNodes[0].childNodes[1].childNodes[2].nodeValue = 'Enable booby mode';
@@ -35,7 +35,7 @@ function createboobyModeLink() {
     document.querySelector('.page-sidemenu ion-list > button:last-of-type ion-icon').classList.add('ion-md-happy');
 }
 function createBoobs() {
-    console.log('execute - createBoobs');
+    // console.log('execute - createBoobs');
     var styleElement = document.createElement('style');
     styleElement.innerHTML = '.page-home .videoparent { top: auto; left: auto; }';
     document.querySelector('.page-home .videoparent').innerHTML = '<img id="gif" class="" src="https://media.giphy.com/media/GP0hgNywFfqz6/200w.webp" srcset="https://media.giphy.com/media/GP0hgNywFfqz6/200w.webp 200w, https://media.giphy.com/media/GP0hgNywFfqz6/giphy.webp 480w" sizes="100vw" alt="">';
