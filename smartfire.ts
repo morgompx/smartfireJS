310f443c2e6133b95abfa5681fea51470a5c03ce
dave morgan <morgo52@gmail.com>
2018-07-04 22:18:29 +1000

    remove comments

--- smartfire.ts
+++ smartfire.ts
@@ -5,21 +5,21 @@ console.log('Invasion successful!');
 updatePageSidemenu();
 
 function updatePageSidemenu() {
-    console.log('execute - updatePageSidemenu');
+    // console.log('execute - updatePageSidemenu');
     removeStupidMeatBackground();
     createHomeLink();
     createboobyModeLink();
 }
 
 function removeStupidMeatBackground() {
-    console.log('execute - removeStupidMeatBackground');
+    // console.log('execute - removeStupidMeatBackground');
     var styleElement = document.createElement('style');
     styleElement.innerHTML = '.header-background-image { background-image: none !important; height: auto !important; }';
     document.querySelector('.header-background-image').appendChild(styleElement);
 }
 
 function createHomeLink() {
-    console.log('execute - createHomeLink');
+    // console.log('execute - createHomeLink');
 
     var homeLink = document.querySelector('.page-sidemenu ion-list > button').cloneNode(true);
     homeLink.addEventListener('click', function() { window.location.href = 'https://app.smartfirebbq.com/#/home';}, false);
@@ -31,7 +31,7 @@ function createHomeLink() {
 }
 
 function createboobyModeLink() {
-    console.log('execute - createboobyModeLink');
+    // console.log('execute - createboobyModeLink');
 
     var boobyLink = document.querySelector('.page-sidemenu ion-list > button').cloneNode(true);
     boobyLink.addEventListener('click', function() { createBoobs(); } , false);
@@ -43,7 +43,7 @@ function createboobyModeLink() {
 }
 
 function createBoobs() {
-    console.log('execute - createBoobs');
+    // console.log('execute - createBoobs');
     var styleElement = document.createElement('style');
     styleElement.innerHTML = '.page-home .videoparent { top: auto; left: auto; }';
     document.querySelector('.page-home .videoparent').innerHTML = '<img id="gif" class="" src="https://media.giphy.com/media/GP0hgNywFfqz6/200w.webp" srcset="https://media.giphy.com/media/GP0hgNywFfqz6/200w.webp 200w, https://media.giphy.com/media/GP0hgNywFfqz6/giphy.webp 480w" sizes="100vw" alt="">';
