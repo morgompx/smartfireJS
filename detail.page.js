8861903771da05470b6f6fa4613e66c75a4cdfae
dave morgan <morgo52@gmail.com>
2018-07-06 18:48:04 +1000

    demodule

--- detail.page.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function styleToolbar() {
-    document.querySelector('.toolbar .toolbar-background').setAttribute('style', 'background-color: #242424 !important; color: #ffffff !important;');
-}
-exports.styleToolbar = styleToolbar;
--- detail.page.ts
+++
@@ -1,3 +0,0 @@
-export function styleToolbar() {
-    document.querySelector('.toolbar .toolbar-background').setAttribute('style', 'background-color: #242424 !important; color: #ffffff !important;');
-}
